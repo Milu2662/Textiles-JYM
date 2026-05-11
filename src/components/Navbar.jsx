0fc67f3d5465e0995ad0c042422bb2a8fdd562e8
Milu2662 <90276444+Milu2662@users.noreply.github.com>
2026-05-11 13:09:34 -0500

    Add logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2>Textiles JYM</h2>
 
-      <div className="nav-links">
-        <a href="#home">Inicio</a>
-        <a href="#about">Sobre Nosotros</a>
-        <a href="#promotion">Promociones</a>
-        <a href="#products">Productos</a>
-        <a href="#contact">Contacto</a>
+      {/* LOGO */}
+      <div className="logo-container">
+        <img
+          src="/logo.svg"
+          alt="Textiles JYM"
+          className="logo"
+        />
+
+        <h2>TEXTILES JYM</h2>
       </div>
+
+      {/* MENÚ */}
+      <ul className="nav-links">
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Promociones</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+      </ul>
+
     </nav>
   )
 }
