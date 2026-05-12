aac2e5ca4eb24de542b79ea62789a9f8f213cfae
Milu2662 <90276444+Milu2662@users.noreply.github.com>
2026-05-12 15:56:16 -0500

    Fix cellphone -version6

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,7 +1,9 @@
 import "./promotion.css";
 
 export default function Promotion() {
+
   return (
+
     <section id="promotion" className="promotion">
 
       <div className="promotion-header">
@@ -13,14 +15,24 @@ export default function Promotion() {
 
         {/* SLIDE 1 */}
         <div className="slide">
-          <div
-            className="slide-bg"
-            style={{
-              backgroundImage: 'url("/promotions/promo2.jpg")'
-            }}
-          ></div>
-        <div className="overlay"></div>
 
+          <picture>
+
+            {/* IMAGEN CELULAR */}
+            <source
+              media="(max-width: 768px)"
+              srcSet="/promotions/promo2-mobile.jpg"
+            />
+
+            {/* IMAGEN DESKTOP */}
+            <img
+              src="/promotions/promo2.jpg"
+              alt="Promoción Polos"
+              className="slide-image"
+            />
+
+          </picture>
+          <div className="overlay"></div>
           <div className="slide-content">
             <span className="badge blue">OFERTA</span>
             <h3>3 POLOS $50.000</h3>
@@ -28,28 +40,33 @@ export default function Promotion() {
           </div>
         </div>
 
-
-
         {/* SLIDE 2 */}
         <div className="slide">
-          <div
-            className="slide-bg"
-            style={{
-              backgroundImage: 'url("/promotions/oferta2.jpg")'
-            }}
-            
-          ></div>
-          <div className="overlay"></div>
 
+          <picture>
+
+            {/* IMAGEN CELULAR */}
+            <source
+              media="(max-width: 768px)"
+              srcSet="/promotions/oferta2-mobile.jpg"
+            />
+
+            {/* IMAGEN DESKTOP */}
+            <img
+              src="/promotions/oferta2.jpg"
+              alt="Promoción Camisetas"
+              className="slide-image"
+            />
+
+          </picture>
+          <div className="overlay"></div>
           <div className="slide-content">
             <span className="badge red">2x1</span>
             <h3>CAMISETAS T-SHIRT</h3>
             <p>Compra 1 y llévate 2</p>
           </div>
         </div>
-
       </div>
-
     </section>
   );
 }
