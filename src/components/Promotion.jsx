463e78d2ef617116f8743f2828989fb71999db5d
Milu2662 <90276444+Milu2662@users.noreply.github.com>
2026-05-12 16:04:32 -0500

    Fix cellphone -version-8

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -32,12 +32,7 @@ export default function Promotion() {
             />
 
           </picture>
-          <div className="overlay"></div>
-          <div className="slide-content">
-            <span className="badge blue">OFERTA</span>
-            <h3>3 POLOS $50.000</h3>
-            <p>Combina estilos y ahorra más</p>
-          </div>
+
         </div>
 
         {/* SLIDE 2 */}
@@ -57,14 +52,7 @@ export default function Promotion() {
               alt="Promoción Camisetas"
               className="slide-image"
             />
-
           </picture>
-          <div className="overlay"></div>
-          <div className="slide-content">
-            <span className="badge red">2x1</span>
-            <h3>CAMISETAS T-SHIRT</h3>
-            <p>Compra 1 y llévate 2</p>
-          </div>
         </div>
       </div>
     </section>
